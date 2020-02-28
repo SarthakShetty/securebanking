38f896b74f07e78a48981de239aa0f4a1b522656
amarnathTadigadapa <46694941+amarnathTadigadapa@users.noreply.github.com>
2020-02-28 12:38:16 -0700

    Update CustomerDAO.java

--- BankingSystem/src/main/java/com/group12/dao/CustomerDAO.java
+++ BankingSystem/src/main/java/com/group12/dao/CustomerDAO.java
@@ -1,6 +1,7 @@
 package com.group12.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.group12.models.Customer;
@@ -10,5 +11,19 @@ public class CustomerDAO {
 
 	@Autowired
 	private Customer customer;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	
+	public void firstQuery() {
+
+	    //jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+	    jdbcTemplate.execute("CREATE TABLE customers(" +
+	        "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+	}
+		
+	
+	
 	
 }
