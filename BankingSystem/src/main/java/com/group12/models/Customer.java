68a8af4607c3951aa19efeb9f8a1a011419631a7
amarnathTadigadapa <46694941+amarnathTadigadapa@users.noreply.github.com>
2020-02-28 12:39:16 -0700

    Update Customer.java

--- BankingSystem/src/main/java/com/group12/models/Customer.java
+++ BankingSystem/src/main/java/com/group12/models/Customer.java
@@ -1,13 +1,26 @@
 package com.group12.models;
 
+import java.sql.Timestamp;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class Customer {
-
-	private String firstName;
 	
+	private String cust_id;
+	private int age;
+	private String firstName;
 	private String lastName;
+	private String email;
+	private Timestamp last_successful_transaction_time;
+	private String mobile;
+	private char type;
+	private String state;
+	private String city;
+	private String address;
+	private int failure_count;
+	
+
 
 	public String getFirstName() {
 		return firstName;
@@ -23,6 +36,86 @@ public class Customer {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Timestamp getLast_successful_transaction_time() {
+		return last_successful_transaction_time;
+	}
+
+	public void setLast_successful_transaction_time(Timestamp last_successful_transaction_time) {
+		this.last_successful_transaction_time = last_successful_transaction_time;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public char getType() {
+		return type;
+	}
+
+	public void setType(char type) {
+		this.type = type;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getFailure_count() {
+		return failure_count;
+	}
+
+	public void setFailure_count(int failure_count) {
+		this.failure_count = failure_count;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCust_id() {
+		return cust_id;
+	}
+
+	public void setCust_id(String cust_id) {
+		this.cust_id = cust_id;
 	}
 	
 }
